c14231677533924cc39cde07ac625038a318ba89
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-03-06 16:59:52 -0500

    added the main.tf and .gitignore

---
+++ infrastructure/main.tf
@@ -0,0 +1,62 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.50"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+#the prefix that goes labels names, its default is set to daig0104 algonquin username
+variable "labelPrefix" {
+ type        = string
+ default     = "daig0104"
+ description = "this is the prefix for the label"
+}
+#variable for region, default set to canada central
+variable "region" {
+ type        = string
+ default     = "Canada Central"
+ description = "the defined region"
+}
+#the variable for admin username, i called it azureadmin for ease of use
+variable "admin_username" {
+ type        = string
+ default     = "azureadmin"
+ description = "the username of admin"
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  # Leave the features block empty to accept all defaults
+  features {}
+}
+
+provider "cloudinit" {
+  # Configuration options
+}
+#creating the resource group
+resource "azurerm_resource_group" "Lab9RG" {
+  name     = "${var.labelPrefix}-A09-RG"
+  location = "${var.region}"
+}
+
+resource "azurerm_storage_account" "lab9Storage" {
+  name                     = "lab9Storage"
+  resource_group_name      = azurerm_resource_group.Lab9RG.name
+  location                 = azurerm_resource_group.Lab9RG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
