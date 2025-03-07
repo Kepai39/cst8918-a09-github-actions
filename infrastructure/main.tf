92aedeea2f1280ae6964393f03762625df528237
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-03-06 23:33:33 -0500

    fixing action issue

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,7 +25,7 @@ variable "labelPrefix" {
 variable "region" {
   type        = string
   default     = "Canada Central"
-  description = "the defined region"
+  description = "defining the region as canada central"
 }
 
 # Define providers and their config params
