6ead4b35fdcde54726cc1bca5162b344a69bd9e6
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-03-06 23:54:44 -0500

    added bad syntax to test syntax validator

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,7 +25,6 @@ variable "labelPrefix" {
 variable "region" {
   type        = string
   default     = "Canada Central"
-  description = "the defined region as Canada Central"
 }
 
 # Define providers and their config params
@@ -44,7 +43,7 @@ resource "azurerm_resource_group" "Lab9RG" {
 }
 
 resource "azurerm_storage_account" "lab9Storage" {
-  name                     = "lab9storagecst8918"
+  name                     = "lab9Storagecst8918"
   resource_group_name      = azurerm_resource_group.Lab9RG.name
   location                 = azurerm_resource_group.Lab9RG.location
   account_tier             = "Standard"
