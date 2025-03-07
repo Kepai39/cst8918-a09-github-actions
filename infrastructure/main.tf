6fa3c8a32e3af0f623043a45a177ff0c4fcb2908
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-03-07 02:00:55 -0500

    I made a mistake in previous test,  This one should output working

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,7 +25,7 @@ variable "labelPrefix" {
 variable "region" {
   type        = string
   default     = "Canada Central"
-  description = "this the location for the resource group"
+  description = "this the resource locations default is canada central"
 }
 
 # Define providers and their config params
